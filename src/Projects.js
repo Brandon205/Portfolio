33097681a13fbfe6f33e2841bc6590155f5d22f5
Brandon205 <brandonblack02@gmail.com>
2019-12-13 16:12:21 -0800

    Project CSS is wacky

--- src/Projects.js
+++ src/Projects.js
@@ -1,4 +1,8 @@
 import React, { useState } from 'react';
+import cubeX from './img/cubeX.png';
+import trailWeather from './img/trailWeather.png';
+import potterGo from './img/potterGo.png';
+import adventureAwaits from './img/adventureAwaits.png';
 
 export default function Projects() {
   const [project1, setProject1] = useState(false);
@@ -10,55 +14,68 @@ export default function Projects() {
   switch (true) {
     case (project1):
       content1 = (
-        <div className="project-details">
-          <h1>cubeX</h1>
-          <hr/>
-          <div className="button">
-            <a href="#">Live Site</a>
+        <>
+        <img className="project-img-blur" src={cubeX} alt="cubeX home page" />
+          <div className="project-details">
+            <h1>cubeX</h1>
+            <hr/>
+            <div className="button">
+              <a href="https://sheltered-reef-38980.herokuapp.com/">Live Site</a>
+            </div>
+            <div className="button">
+              <a href="https://github.com/Brandon205/cubeX" rel="noopener-noreferrer">Code</a>
+            </div>
           </div>
-          <div className="button">
-            <a href="#">Code</a>
-          </div>
-        </div>
+        </>
       );
       break;
     case (project2):
       content2 = (
-        <div className="project-details">
-        <h1>PotterGo</h1>
-        <hr/>
-        <div className="button">
-          <a href="#">Code</a>
-        </div>
-      </div>
+        <>
+          <img className="project-img-blur" src={potterGo} alt="Potter Go home page" />
+          <div className="project-details">
+            <h1>PotterGo</h1>
+            <hr/>
+            <div className="button">
+              <a href="https://github.com/Brandon205/PotterGo">Code</a>
+            </div>
+          </div>
+        </>
       );
       break;
     case (project3):
       content3 = (
-        <div className="project-details">
-        <h1>Adventure Awaits</h1>
-        <hr/>
-        <div className="button">
-          <a href="#">Live Site</a>
-        </div>
-        <div className="button">
-          <a href="#">Code</a>
-        </div>
-      </div>
+        <>
+          <img className="project-img-blur" src={adventureAwaits} alt="Adventure Awaits home page" />
+          <div className="project-details">
+          <h1>Adventure Awaits</h1>
+          <hr/>
+          <div className="button">
+            <a href="https://boiling-depths-32027.herokuapp.com/">Live Site</a>
+          </div>
+            <div className="button">
+              <a href="https://github.com/Brandon205/adventure-awaits">Code</a>
+            </div>
+          </div>
+        </>
       );
       break;
     case (project4):
       content4 = (
-        <div className="project-details">
-        <h1>Trail Weather</h1>
-        <hr/>
-        <div className="button">
-          <a href="#">Live Site</a>
-        </div>
-        <div className="button">
-          <a href="#">Code</a>
-        </div>
-      </div>
+        <>
+          <img className="project-img-blur" src={trailWeather} alt="Trail Weather home page" />
+          <div className="project-details">
+          <h1>Trail Weather</h1>
+          <hr/>
+          <div className="button">
+            <a href="https://fast-coast-44019.herokuapp.com/">Live Site</a>
+          </div>
+            <div className="button">
+              <a href="https://github.com/Brandon205/TrailWeather">Code</a>
+            </div>
+          </div>
+          <p>A MERN app that uses GraphQL and Apollo for all of the database calls and manipulations along with React with hooks for the front end. It is a simple app that will allow users to time themselves while solving a Rubik’s cube, it also has many data-analysis features like averages and a graph, for the user to track their times in more visual ways.</p>
+        </>
       );
       break;
       default:
@@ -68,23 +85,27 @@ export default function Projects() {
         content4 = '';
   }
   return (
-    <div className="projects">
+    <div id="projects">
       <h1>Projects</h1>
       <main className="project-container">
         <div>
           <div className="project" onMouseEnter={() => setProject1(true)} onMouseLeave={() => setProject1(false)}>
-            {content1}
+            {project1 ? content1 : <img className="project-img" src={cubeX} alt="cubeX home page" /> }
+            <p>A MERN app that uses GraphQL and Apollo for all of the database calls and manipulations along with React with hooks for the front end. It is a simple app that will allow users to time themselves while solving a Rubik’s cube, it also has many data-analysis features like averages and a graph, for the user to track their times in more visual ways.</p>
           </div>
           <div className="project" onMouseEnter={() => setProject2(true)} onMouseLeave={() => setProject2(false)}>
-            {content2}
+            {project2 ? content2 : <img className="project-img" src={potterGo} alt="Potter Go home page" /> }
+            <p>A 1 day hackathon project that was made by me and one other developer, along with a group of 3 UX Designers. It is a simplistic MERN app that uses MapBox to place images at certain locations that the user would have to go to. It also reaches out to an external Harry Potter api to get information, like wands, from the Harry Potter series. Our project placed 1st in the hackathon.</p>
           </div>
         </div>
         <div>
           <div className="project" onMouseEnter={() => setProject3(true)} onMouseLeave={() => setProject3(false)}>
-            {content3}
+            {project3 ? content3 : <img className="project-img" src={adventureAwaits} alt="Adventure Awaits home page" /> }
+            <p>A MongoDB, Express, React, Node.js (MERN) app, that was created by a team of developers. Using React for the front end made the website very quick and responsive, while on the backend Node, Express, and MongoDB handle a JWT authentication system along with that will allow for users to have their own secure accounts.</p>
           </div>
           <div className="project" onMouseEnter={() => setProject4(true)} onMouseLeave={() => setProject4(false)}>
-            {content4}
+            {project4 ? content4 : <img className="project-img" src={trailWeather} alt="Trail Weather home page" /> }
+            <p>A full stack web app that is used to help plan your hikes more accordingly with the weather. This app uses HTML, a templating language called EJS, CSS, and Javascript to lay out the foundation along with a database and 3 APIs including Mapbox and the DarkSky API to provide information to the user about any weather or hike information they need.</p>
           </div>
         </div>
       </main>
