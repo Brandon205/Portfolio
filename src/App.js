ea8ebece658c57520136524d513d1b678e7c14f7
Brandon205 <brandonblack02@gmail.com>
2019-12-17 09:54:13 -0800

    Final content add styled skills & projects further

--- src/App.js
+++ src/App.js
@@ -28,8 +28,8 @@ export default function App() {
       <header>
         <div>
           <h1 className="name" id="main"> <span className="bouncy">&lt;</span><span className="bouncy">B</span><span className="bouncy">r</span><span className="bouncy">a</span><span className="bouncy">n</span><span className="bouncy">d</span><span className="bouncy">o</span><span className="bouncy">n</span> <span className="bouncy">B</span><span className="bouncy">l</span><span className="bouncy">a</span><span className="bouncy">c</span><span className="bouncy">k</span><span className="bouncy">/</span><span className="bouncy">&gt;</span> </h1>
-          <h4>Full Stack Web Developer</h4>
         </div>
+          <h4>A Seattle-based Full Stack Web Developer</h4>
       </header>
       <main>
         <Skills />
@@ -37,9 +37,9 @@ export default function App() {
       <h1 id="contact">Contact</h1>
       <p>Feel free to email me at brandonblack02@gmail.com</p>
       <div className="icons">
-        <a href="https://twitter.com/brandonblack02" className="fa fa-twitter"></a>
-        <a href="https://www.linkedin.com/in/brandonblack02/" className="fa fa-linkedin"></a>
-        <a href="https://github.com/Brandon205" className="fa fa-github"></a>
+        <a href="https://twitter.com/brandonblack02" className="fa fa-twitter" target="_blank" rel="noopener noreferrer"></a>
+        <a href="https://www.linkedin.com/in/brandonblack02/" className="fa fa-linkedin" target="_blank" rel="noopener noreferrer"></a>
+        <a href="https://github.com/Brandon205" className="fa fa-github" target="_blank" rel="noopener noreferrer"></a>
       </div>
       </main>
     </div>
--- src/Skills.js
+++ src/Skills.js
@@ -1,28 +1,24 @@
 import React from 'react';
+import mongoDB from './img/mongodb-icon.svg';
+import graphQL from './img/graphql-icon.svg';
+import nodeJS from './img/nodejs-icon.svg';
 
 export default function Skills() {
   return (
     <div className="skills" id="skills">
       <h1>Skills</h1>
       <hr />
-      <h3>Front End -</h3>
-      <div>
-        <p>JavaScript</p>
-        <p>React</p>
-        <p>Apollo</p>
-        <p>TypeScript</p>
-        <p>HTML5</p>
-        <p>CSS3</p>
+      <div className="skill-img-div">
+        <a className="skill-atag" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noopener noreferrer"><i className="fab fa-js"></i></a>
+        <a className="skill-atag" href="https://reactjs.org/" target="_blank" rel="noopener noreferrer"><i className="fab fa-react"></i></a>
+        <a className="skill-atag" href="https://graphql.org/" target="_blank" rel="noopener noreferrer"><img className="fab fa" src={graphQL} alt="mongodb logo"/></a>
+        <a className="skill-atag" href="https://nodejs.org/en/" target="_blank" rel="noopener noreferrer"><img className="skill-icon" src={nodeJS} alt="mongodb logo"/></a>
+        <a className="skill-atag" href="https://www.mongodb.com/" target="_blank" rel="noopener noreferrer"><img className="skill-icon" src={mongoDB} alt="mongodb logo"/></a>
+        <a className="skill-atag" href="https://www.python.org/" target="_blank" rel="noopener noreferrer"><i className="fab fa-python"></i></a>
+        <a className="skill-atag" href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="_blank" rel="noopener noreferrer"><i className="fab fa-html5"></i></a>
+        <a className="skill-atag" href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank" rel="noopener noreferrer"><i className="fab fa-css3"></i></a>
       </div>
-      <h3>Back End -</h3>
-      <div>
-        <p>Python</p>
-        <p>SQL</p>
-        <p>MongoDB</p>
-        <p>Node</p>
-        <p>Express</p>
-        <p>GraphQL</p>
-      </div>
+      <p>For a full list of my skills and qualifications check out my <a className="resume" href="">resume</a>.</p>
     </div>
   )
 }
