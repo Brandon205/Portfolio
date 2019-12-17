7e1c2e72a8d5f99c8b4a1c72b281d6910dba7c48
Brandon205 <brandonblack02@gmail.com>
2019-12-17 14:19:55 -0800

    Updates

---
+++ precache-manifest.2fac8f1f841f6e1c78b9501806b9c5a5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb8b702d256b0bd14ca6c47a9ab17987",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "fc164232728e4c86f1a3",
+    "url": "/Portfolio/static/css/main.4921e0ee.chunk.css"
+  },
+  {
+    "revision": "88d7b21a1184ec1d5243",
+    "url": "/Portfolio/static/js/2.0d0dd4cf.chunk.js"
+  },
+  {
+    "revision": "fc164232728e4c86f1a3",
+    "url": "/Portfolio/static/js/main.dbae2c9a.chunk.js"
+  },
+  {
+    "revision": "dd65a4e71a8b70668f2c",
+    "url": "/Portfolio/static/js/runtime-main.6a869909.js"
+  },
+  {
+    "revision": "7d01c5c61f9547c414fb268ff09f25db",
+    "url": "/Portfolio/static/media/Resume.7d01c5c6.pdf"
+  },
+  {
+    "revision": "aa270455babe2906af307117262fc8d0",
+    "url": "/Portfolio/static/media/adventureAwaits.aa270455.png"
+  },
+  {
+    "revision": "8c23b369df26f822fe5c3391e908e13d",
+    "url": "/Portfolio/static/media/conifer-daylight-evergreen-forest-23381.8c23b369.jpg"
+  },
+  {
+    "revision": "39a14eeda378266cca5494b3756f72fd",
+    "url": "/Portfolio/static/media/cubeX.39a14eed.png"
+  },
+  {
+    "revision": "32bc428f67abce6b369e0d6cf224f28a",
+    "url": "/Portfolio/static/media/graphql-icon.32bc428f.svg"
+  },
+  {
+    "revision": "554c7d4bfd63a3da33773978fd291c06",
+    "url": "/Portfolio/static/media/mongodb-icon.554c7d4b.svg"
+  },
+  {
+    "revision": "5c376133d6403088a87a04fcc9685674",
+    "url": "/Portfolio/static/media/nodejs-icon.5c376133.svg"
+  },
+  {
+    "revision": "9b7b0c27d276145b1e8e1e82bda32946",
+    "url": "/Portfolio/static/media/potterGo.9b7b0c27.png"
+  },
+  {
+    "revision": "0ec6c09fc8c150bda97d689230239d29",
+    "url": "/Portfolio/static/media/trailWeather.0ec6c09f.png"
+  }
+]);
