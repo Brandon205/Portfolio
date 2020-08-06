3b84be23d268fb893ed4240b4d617362ee9e457b
Brandon Black <brandonblack02@gmail.com>
2020-08-06 10:02:53 -0700

    Code cleanup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Projects from './Projects';
 import Skills from './Skills';
 import ReactGA from 'react-ga';
@@ -8,32 +8,17 @@ const trackingId = "UA-118954435-2";
 ReactGA.initialize(trackingId);
 
 export default function App() {
-  const [nav, setNav] = useState('none')
-
   return (
     <div className="App">
       <header id="main">
-        <nav className="mobile">
-          {/* <div className="nav-name-div">
-            <a href="#main"><span className="nav-name">Brandon</span></a>
-          </div> */}
-          {/* <div style={{'display': nav}} className="links">
-            <a href="#skills" onClick={() => setNav(nav === 'none' ? "inline" : 'none')}>Skills</a>
-            <a href="#projects" onClick={() => setNav(nav === 'none' ? "inline" : 'none')}>Projects</a>
-            <a href="#contact" onClick={() => setNav(nav === 'none' ? "inline" : 'none')}>Contact</a>
-          </div>
-          <div className="hamburger" onClick={() => setNav(nav === 'none' ? "inline" : 'none')}><i className="fa fa-bars"></i></div> */}
-        </nav>
         <div>
           <h1 className="name"> <span className="bouncy">&lt;</span><span className="bouncy">B</span><span className="bouncy">r</span><span className="bouncy">a</span><span className="bouncy">n</span><span className="bouncy">d</span><span className="bouncy">o</span><span className="bouncy">n</span> <span className="bouncy">B</span><span className="bouncy">l</span><span className="bouncy">a</span><span className="bouncy">c</span><span className="bouncy">k</span><span className="bouncy">/</span><span className="bouncy">&gt;</span> </h1>
         </div>
         <h4>A Seattle-based Full Stack Web Developer</h4>
         <nav>
-          <div className="desktop">
-            <a href="#skills">Skills</a>
-            <a href="#projects">Projects</a>
-            <a href="#contact">Contact</a>
-          </div>
+          <a href="#skills">Skills</a>
+          <a href="#projects">Projects</a>
+          <a href="#contact">Contact</a>
         </nav>
       </header>
       <main>
