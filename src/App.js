f5911996524cf0789e777ce5e971b2ccebe548a4
Brandon Black <brandonblack02@gmail.com>
2020-08-06 09:57:51 -0700

    Cleaner Nav bar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/anchor-has-content */
 import React, { useState } from 'react';
 import Projects from './Projects';
 import Skills from './Skills';
@@ -13,27 +12,29 @@ export default function App() {
 
   return (
     <div className="App">
-      <nav className="mobile">
-        <div className="nav-name-div">
-          <a href="#main"><span className="nav-name">Brandon</span></a>
-        </div>
-        <div style={{'display': nav}} className="links">
-          <a href="#skills" onClick={() => setNav(nav === 'none' ? "inline" : 'none')}>Skills</a>
-          <a href="#projects" onClick={() => setNav(nav === 'none' ? "inline" : 'none')}>Projects</a>
-          <a href="#contact" onClick={() => setNav(nav === 'none' ? "inline" : 'none')}>Contact</a>
-        </div>
-        <div className="hamburger" onClick={() => setNav(nav === 'none' ? "inline" : 'none')}><i className="fa fa-bars"></i></div>
-        <div className="desktop">
-          <a href="#skills">Skills</a>&nbsp;&nbsp;&nbsp;&nbsp;
-          <a href="#projects">Projects</a>&nbsp;&nbsp;&nbsp;&nbsp;
-          <a href="#contact">Contact</a>
-        </div>
-      </nav>
-      <header  id="main">
+      <header id="main">
+        <nav className="mobile">
+          {/* <div className="nav-name-div">
+            <a href="#main"><span className="nav-name">Brandon</span></a>
+          </div> */}
+          {/* <div style={{'display': nav}} className="links">
+            <a href="#skills" onClick={() => setNav(nav === 'none' ? "inline" : 'none')}>Skills</a>
+            <a href="#projects" onClick={() => setNav(nav === 'none' ? "inline" : 'none')}>Projects</a>
+            <a href="#contact" onClick={() => setNav(nav === 'none' ? "inline" : 'none')}>Contact</a>
+          </div>
+          <div className="hamburger" onClick={() => setNav(nav === 'none' ? "inline" : 'none')}><i className="fa fa-bars"></i></div> */}
+        </nav>
         <div>
           <h1 className="name"> <span className="bouncy">&lt;</span><span className="bouncy">B</span><span className="bouncy">r</span><span className="bouncy">a</span><span className="bouncy">n</span><span className="bouncy">d</span><span className="bouncy">o</span><span className="bouncy">n</span> <span className="bouncy">B</span><span className="bouncy">l</span><span className="bouncy">a</span><span className="bouncy">c</span><span className="bouncy">k</span><span className="bouncy">/</span><span className="bouncy">&gt;</span> </h1>
         </div>
-          <h4>A Seattle-based Full Stack Web Developer</h4>
+        <h4>A Seattle-based Full Stack Web Developer</h4>
+        <nav>
+          <div className="desktop">
+            <a href="#skills">Skills</a>
+            <a href="#projects">Projects</a>
+            <a href="#contact">Contact</a>
+          </div>
+        </nav>
       </header>
       <main>
         <Skills />
