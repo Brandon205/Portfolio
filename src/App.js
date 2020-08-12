2155ea55d087b60525caf57d399e214af783cbc0
Brandon Black <brandonblack02@gmail.com>
2020-08-11 18:34:56 -0700

    Gave links aria-labels

--- src/App.js
+++ src/App.js
@@ -55,9 +55,9 @@ export default function App() {
           <hr />
           <p>Feel free to send me a message on LinkedIn or email me at brandonblack02@gmail.com</p>
           <div className="icons">
-            <a href="https://twitter.com/brandonblack02" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter"></i></a>
-            <a href="https://www.linkedin.com/in/brandonblack02/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
-            <a href="https://github.com/Brandon205" target="_blank" rel="noopener noreferrer"><i className="fab fa-github-square social-icons"></i></a>
+            <a href="https://twitter.com/brandonblack02" target="_blank" rel="noopener noreferrer" aria-label="Twitter"><i className="fab fa-twitter"></i></a>
+            <a href="https://www.linkedin.com/in/brandonblack02/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn"><i className="fab fa-linkedin"></i></a>
+            <a href="https://github.com/Brandon205" target="_blank" rel="noopener noreferrer" aria-label="Github"><i className="fab fa-github-square social-icons"></i></a>
           </div>
         </div>
       </main>
--- src/Skills.js
+++ src/Skills.js
@@ -10,14 +10,14 @@ export default function Skills() {
       <h1 className="sub-heading">Skills</h1>
       <hr />
       <div>
-        <a className="skill-atag" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noopener noreferrer"><i className="fab fa-js"></i></a>
-        <a className="skill-atag" href="https://reactjs.org/" target="_blank" rel="noopener noreferrer"><i className="fab fa-react"></i></a>
-        <a className="skill-atag" href="https://graphql.org/" target="_blank" rel="noopener noreferrer"><img className="skill-icon-img" src={graphQL} alt="mongodb logo"/></a>
-        <a className="skill-atag" href="https://nodejs.org/en/" target="_blank" rel="noopener noreferrer"><img className="skill-icon-img" src={nodeJS} alt="mongodb logo"/></a>
-        <a className="skill-atag" href="https://www.mongodb.com/" target="_blank" rel="noopener noreferrer"><img className="skill-icon-img" src={mongoDB} alt="mongodb logo"/></a>
-        <a className="skill-atag" href="https://www.python.org/" target="_blank" rel="noopener noreferrer"><i className="fab fa-python"></i></a>
-        <a className="skill-atag" href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="_blank" rel="noopener noreferrer"><i className="fab fa-html5"></i></a>
-        <a className="skill-atag" href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank" rel="noopener noreferrer"><i className="fab fa-css3"></i></a>
+        <a className="skill-atag" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noopener noreferrer" aria-label="Javascript"><i className="fab fa-js"></i></a>
+        <a className="skill-atag" href="https://reactjs.org/" target="_blank" rel="noopener noreferrer" aria-label="React"><i className="fab fa-react"></i></a>
+        <a className="skill-atag" href="https://graphql.org/" target="_blank" rel="noopener noreferrer" aria-label="GraphQL"><img className="skill-icon-img" src={graphQL} alt="mongodb logo"/></a>
+        <a className="skill-atag" href="https://nodejs.org/en/" target="_blank" rel="noopener noreferrer" aria-label="Node.js"><img className="skill-icon-img" src={nodeJS} alt="mongodb logo"/></a>
+        <a className="skill-atag" href="https://www.mongodb.com/" target="_blank" rel="noopener noreferrer" aria-label="MongoDB"><img className="skill-icon-img" src={mongoDB} alt="mongodb logo"/></a>
+        <a className="skill-atag" href="https://www.python.org/" target="_blank" rel="noopener noreferrer" aria-label="Python"><i className="fab fa-python"></i></a>
+        <a className="skill-atag" href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="_blank" rel="noopener noreferrer" aria-label="HTML"><i className="fab fa-html5"></i></a>
+        <a className="skill-atag" href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank" rel="noopener noreferrer" aria-label="CSS"><i className="fab fa-css3"></i></a>
       </div>
       <p>For a full list of my skills and qualifications check out my <a className="resume" href={resume} target="_blank" rel="noopener noreferrer">resume</a>.</p>
     </div>
