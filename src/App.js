647aa08248eb428578b8855c162c0071afe2469e
Brandon Black <brandonblack02@gmail.com>
2020-08-11 18:27:13 -0700

    More Mobile friendly Nav bar

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ export default function App() {
         <div className="contact">
           <h1 id="contact">Contact</h1>
           <hr />
-          <p>Feel free to email me at brandonblack02@gmail.com</p>
+          <p>Feel free to send me a message on LinkedIn or email me at brandonblack02@gmail.com</p>
           <div className="icons">
             <a href="https://twitter.com/brandonblack02" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter"></i></a>
             <a href="https://www.linkedin.com/in/brandonblack02/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
