c48cede339bb1964652a70d6217452fdcd5a4cf3
Brandon Black <53108034+Brandon205@users.noreply.github.com>
2021-05-27 21:07:29 -0700

    Updates

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className="App">
         <main>
           <Route exact path="/" component={Skills} />
