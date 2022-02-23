cb80c16d7a793d03fa7fd61e411bf5835cd2d652
Brandon Black <53108034+Brandon205@users.noreply.github.com>
2022-02-22 18:23:22 -0800

    Updated Projects

--- src/Projects.js
+++ src/Projects.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import bid from './img/Bid-Summary.webp'
-import trailWeather from './img/trailWeather.webp';
+// import trailWeather from './img/trailWeather.webp';
+import guitario from './img/guitario.webp';
 import potterGo from './img/potterGo.webp';
 import bidOld from './img/Bid-Summary.png';
 import hueLite from './img/HueLite.webp';
-import trailWeatherOld from './img/trailWeather.png';
+// import trailWeatherOld from './img/trailWeather.png';
+import guitarioOld from './img/guitario.png';
 import potterGoOld from './img/potterGo.png';
 import hueLiteOld from './img/HueLite.png';
 
@@ -30,7 +32,7 @@ export default function Projects() {
             <img className="project-img-blur" src={bidOld} alt="cubeX home page" />
           </picture>
           <div className="project-details">
-            <h1>PrimeBid Excel</h1>
+            <h1 className="project-title">PrimeBid Excel</h1>
             <hr />
             <div className="button">
               <Link to="/bidding-software" onClick={scrollTop}>Example</Link>
@@ -43,15 +45,18 @@ export default function Projects() {
       content2 = (
         <>
           <picture>
-            <source srcSet={potterGo} type="image/webp" />
-            <source srcSet={potterGoOld} type="image/jpeg" /> 
-            <img className="project-img-blur" src={potterGoOld} alt="Potter Go home page" />
+            <source srcSet={guitario} type="image/webp" />
+            <source srcSet={guitarioOld} type="image/jpeg" /> 
+            <img className="project-img-blur" src={guitarioOld} alt="Trail Weather home page" />
           </picture>
           <div className="project-details">
-            <h1>PotterGo</h1>
-            <hr />
+          <h1 className="project-title">Guitario</h1>
+          <hr />
+          <div className="button">
+            <a href="https://brandon205.github.io/guitario/" target="_blank" rel="noopener noreferrer">Live Demo</a>
+          </div>
             <div className="button">
-              <a href="https://github.com/Brandon205/PotterGo" target="_blank" rel="noopener noreferrer">Github</a>
+              <a href="https://github.com/Brandon205/guitario" target="_blank" rel="noopener noreferrer">Github</a>
             </div>
           </div>
         </>
@@ -66,7 +71,7 @@ export default function Projects() {
             <img className="project-img-blur" src={hueLiteOld} alt="Adventure Awaits home page" />
           </picture>
           <div className="project-details">
-          <h1>Hue Lite</h1>
+          <h1 className="project-title">Hue Lite</h1>
           <hr />
           <div className="button">
             <a href="https://brandon205.github.io/hue-lights" target="_blank" rel="noopener noreferrer">Live Demo</a>
@@ -82,18 +87,15 @@ export default function Projects() {
       content4 = (
         <>
           <picture>
-            <source srcSet={trailWeather} type="image/webp" />
-            <source srcSet={trailWeatherOld} type="image/jpeg" /> 
-            <img className="project-img-blur" src={trailWeatherOld} alt="Trail Weather home page" />
+            <source srcSet={potterGo} type="image/webp" />
+            <source srcSet={potterGoOld} type="image/jpeg" /> 
+            <img className="project-img-blur" src={potterGoOld} alt="Potter Go home page" />
           </picture>
           <div className="project-details">
-          <h1>Trail Weather</h1>
-          <hr />
-          <div className="button">
-            <a href="https://fast-coast-44019.herokuapp.com/" target="_blank" rel="noopener noreferrer">Live Demo</a>
-          </div>
+            <h1 className="project-title">PotterGo</h1>
+            <hr />
             <div className="button">
-              <a href="https://github.com/Brandon205/TrailWeather" target="_blank" rel="noopener noreferrer">Github</a>
+              <a href="https://github.com/Brandon205/PotterGo" target="_blank" rel="noopener noreferrer">Github</a>
             </div>
           </div>
         </>
@@ -105,6 +107,7 @@ export default function Projects() {
         content3 = ''
         content4 = ''
   }
+
   return (
     <div id="projects" className="projects">
       <h1 className="sub-heading">Projects</h1>
@@ -126,18 +129,17 @@ export default function Projects() {
         </div>
         <div className="project2" onMouseEnter={() => setProject2(true)} onMouseLeave={() => setProject2(false)}>
           {project2 ? content2 : <picture>
-            <source srcSet={potterGo} type="image/webp" />
-            <source srcSet={potterGoOld} type="image/jpeg" /> 
-            <img className="project-img" src={potterGoOld} alt="Potter Go home page" />
+            <source srcSet={guitario} type="image/webp" />
+            <source srcSet={guitarioOld} type="image/jpeg" /> 
+            <img className="project-img" src={guitarioOld} alt="Potter Go home page" />
           </picture> }
         </div>
         <div className="project2-description">
           <section className="frameworks">
-            <p>React</p>
-            <p>Node.js</p>
-            <p>Express</p>
+            <p>React Native</p>
+            <p>Web Audio API</p>
           </section>
-          <p>A 2 day hackathon project that was made by me and one other developer, along with a group of 3 UX Designers. It is a simplistic MERN app that uses MapBox to place images at certain locations that the user would have to go to. It also reaches out to an external Harry Potter api to get information, like wands, from the Harry Potter series. Our project placed 1st in the hackathon.</p>
+          <p>Small and lightweight app that works on Android, IOS, and the Web. Built mostly with React Native, it is designed to help users learn the fretboard of a guitar. Uses the Web Audio API to listen and analyze user audio that can check if they are playing the right note, and it will also display the current note the user is playing.</p>
         </div>
         <div className="project3" onMouseEnter={() => setProject3(true)} onMouseLeave={() => setProject3(false)}>
           {project3 ? content3 : <picture>
@@ -156,9 +158,9 @@ export default function Projects() {
         </div>
         <div className="project4" onMouseEnter={() => setProject4(true)} onMouseLeave={() => setProject4(false)}>
           {project4 ? content4 : <picture>
-            <source srcSet={trailWeather} type="image/webp" />
-            <source srcSet={trailWeatherOld} type="image/jpeg" /> 
-            <img className="project-img" src={trailWeatherOld} alt="Trail Weather home page" />
+            <source srcSet={potterGo} type="image/webp" />
+            <source srcSet={potterGoOld} type="image/jpeg" /> 
+            <img className="project-img" src={potterGoOld} alt="Trail Weather home page" />
           </picture> }
         </div>
         <div className="project4-description">
@@ -167,7 +169,7 @@ export default function Projects() {
             <p>Node.js</p>
             <p>Express</p>
           </section>
-          <p>A full stack web app that is used to help plan your hikes more accordingly with the weather. This app uses HTML, a templating language called EJS, CSS, and Javascript to lay out the foundation along with a database and 3 APIs including Mapbox and the DarkSky API to provide information to the user about any weather or hike information they need.</p>
+          <p>A 2 day hackathon project that was made by me and one other developer, along with a group of 3 UX Designers. It is a simplistic MERN app that uses MapBox to place images at certain locations that the user would have to go to. It also reaches out to an external Harry Potter api to get information, like wands, from the Harry Potter series. Our project placed 1st in the hackathon.</p>
         </div>
       </main>
     </div>
