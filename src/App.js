b599e0f7889108e47d53ed8dc6701f9c12e71fb0
Brandon Black <53108034+Brandon205@users.noreply.github.com>
2022-10-31 14:45:27 -0700

    CSS updates

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Bidding from './Bidding';
 import Skills from './Skills';
 import './App.css';
 
+import { FaTwitter, FaLinkedin, FaGithub } from 'react-icons/fa';
+
 export default function App() {
   return (
     <HashRouter basename={process.env.PUBLIC_URL}>
@@ -11,16 +13,16 @@ export default function App() {
         <main>
           <Route exact path="/" component={Skills} />
           <Route exact path="/bidding-software" component={Bidding} />
-          <div className="contact">
+          <footer>
             <h1 id="contact">Contact</h1>
             <hr />
             <p>Feel free to send me a message on LinkedIn or email me at brandonblack02@gmail.com</p>
             <div className="icons">
-              <a href="https://twitter.com/brandonblack02" target="_blank" rel="noopener noreferrer" aria-label="Twitter"><i className="fab fa-twitter"></i></a>
-              <a href="https://www.linkedin.com/in/brandonblack02/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn"><i className="fab fa-linkedin"></i></a>
-              <a href="https://github.com/Brandon205" target="_blank" rel="noopener noreferrer" aria-label="Github"><i className="fab fa-github-square social-icons"></i></a>
+              <a href="https://twitter.com/brandonblack02" className="social-icon" target="_blank" rel="noopener noreferrer" aria-label="Twitter"><FaTwitter /></a>
+              <a href="https://www.linkedin.com/in/brandonblack02/" className="social-icon" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn"><FaLinkedin /></a>
+              <a href="https://github.com/Brandon205" className="social-icon" target="_blank" rel="noopener noreferrer" aria-label="Github"><FaGithub /></a>
             </div>
-          </div>
+          </footer>
         </main>
       </div>
     </HashRouter>
